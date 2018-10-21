9340e3e8c380ee5199c28e9c26edffe357970eb7
Pierrot727 <pe.laporte@gmail.com>
2018-10-21 20:26:27 +0200

    Passe2

---
+++ fichier.php
@@ -0,0 +1,12 @@
+<?php
+
+class test
+{
+
+    public function test()
+    {
+        $test = "valeur1";
+        return $test;
+    }
+}
+
---
+++ script1.js
@@ -0,0 +1,7 @@
+/*!
+ * Ceci est un baratin
+ */
+
+if (typeof jQuery === 'undefined') {
+    throw new Error('JQyery n est pas defini')
+}
