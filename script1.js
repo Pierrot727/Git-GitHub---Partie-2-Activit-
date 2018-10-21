8dde13c2a22bbdbde2949d9d22b7272298d792ad
Pierrot727 <pe.laporte@gmail.com>
2018-10-21 20:28:57 +0200

    passe 4

--- script1.js
+++ script1.js
@@ -3,9 +3,9 @@
  */
 
 if (typeof jQuery === 'undefined') {
-    throw new Error('JQyery n est pas defini')
+    throw new Error('JQyery n est pas defini c est vrai')
 }
 
 /*
-Ceci est le baratin modifié aprés la passe 2 (donc la 3)
+Ceci est le baratin modifié aprés la passe 3 (donc la 4)
  */
